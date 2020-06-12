7d9b471c00c26d8e1df8aa79ea78cff142b81a35
muhyidinahmad <ahmad19.muhyidin@gmail.com>
2020-06-12 23:24:46 +0700

    :fire: remove unknown fuck ads

--- js/resume.js
+++ js/resume.js
@@ -31,16 +31,33 @@ $(document).ready(function () {
   function removeAds() {
     var iframes = document.querySelectorAll('iframe');
     for (var i = 0; i < iframes.length; i++) {
+      console.log('frames found ');
       iframes[i].parentNode.removeChild(iframes[i]);
     }
     if ($("#rtaFrame").length > 0) {
       $("#rtaFrame").remove();
     }
   }
+  function removeAds2() {
+    var iframes2 = parent.document.querySelectorAll('iframe');
+    for (var i = 0; i < iframes2.length; i++) {
+      console.log('frames found 2');
+      iframes2[i].parentNode.removeChild(iframes2[i]);
+    }
+  }
+  function removeAds3() {
+    var iframes3 = top.document.querySelectorAll('iframe');
+    for (var i = 0; i < iframes3.length; i++) {
+      console.log('frames found 3');
+      iframes3[i].parentNode.removeChild(iframes3[i]);
+    }
+  }
   for(var i = 1000; i <= 30000; i+=1000) {
     setTimeout(function() {
       console.log('remove ads per second');
       removeAds();
+      removeAds2();
+      removeAds3();
     }, i);
   }
 });
